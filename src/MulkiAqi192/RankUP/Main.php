3ca85631ec78ee8fbd8aa1e620f466b7f4e48051
BLAST1718 <71514704+BLAST1718@users.noreply.github.com>
2020-12-06 15:02:14 +0800

    Update Main.php

--- src/MulkiAqi192/RankUP/Main.php
+++ src/MulkiAqi192/RankUP/Main.php
@@ -48,148 +48,106 @@ class main extends PluginBase implements Listener {
 					$knight = $papi->getGroup("Knight");
 					$king = $papi->getGroup("King");
 					$emperor = $papi->getGroup("Emperor");
-					$vip = $papi->getGroup("Vip");
-					$mvp = $papi->getGroup("Mvp");
 					$dragon = $papi->getGroup("DRAGON");
 					$guest = $papi->getGroup("Guest");
 					$crank = $papi->getUserDataMgr()->getGroup($player)->getName();
 					$money = EconomyAPI::getInstance()->myMoney($player);
 					if($crank == "Guest"){
-						if($money > 3000){
-							EconomyAPI::getInstance()->reduceMoney($player, "3000");
+						if($money > 5000){
+							EconomyAPI::getInstance()->reduceMoney($player, "5000");
 							$ppapi->setGroup($player, $warrior);
 							$player->sendMessage("§bYour rank has been upgraded to §6Warrior");
 							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
 							return true;
 						}
-						if($money == 3000){
-							EconomyAPI::getInstance()->reduceMoney($player, "3000");
+						if($money == 5000){
+							EconomyAPI::getInstance()->reduceMoney($player, "5000");
 							$papi->setGroup($player, $warrior);
 							$player->sendMessage("§bYour rank has been upgraded to §6Warrior");
 							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
 							return true;
 						}
-						if($money < 3000){
+						if($money < 5000){
 							$player->sendMessage("§cYou dont have enough money!");
 							return true;
 						}
 					}
 					if($crank == "Warrior"){
-						if($money > 6000){
-							EconomyAPI::getInstance()->reduceMoney($player, "6000");
+						if($money > 10000){
+							EconomyAPI::getInstance()->reduceMoney($player, "10000");
 							$papi->setGroup($player, $knight);
 							$player->sendMessage("§bYour rank has been upgraded to §6Knight");
 							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
 							return true;
 						}
-						if($money == 6000){
-							EconomyAPI::getInstance()->reduceMoney($player, "6000");
+						if($money == 10000){
+							EconomyAPI::getInstance()->reduceMoney($player, "10000");
 							$papi->setGroup($player, $knight);
 							$player->sendMessage("§bYour rank has been upgraded to §6Knight");
 							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
 							return true;
 						}
-						if($money < 6000){
+						if($money < 10000){
 							$player->sendMessage("§cYou dont have enough money!");
 							return true;
 						}
 					}
 					if($crank == "Knight"){
-						if($money > 9000){
-							EconomyAPI::getInstance()->reduceMoney($player, "9000");
+						if($money > 15000){
+							EconomyAPI::getInstance()->reduceMoney($player, "15000");
 							$papi->setGroup($player, $king);
 							$player->sendMessage("§bYour rank has been upgraded to §6King");
 							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
 							return true;
 						}
-						if($money == 9000){
-							EconomyAPI::getInstance()->reduceMoney($player, "9000");
+						if($money == 15000){
+							EconomyAPI::getInstance()->reduceMoney($player, "15000");
 							$papi->setGroup($player, $king);
 							$player->sendMessage("§bYour rank has been upgraded to §6King");
 							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
 							return true;
 						}
-						if($money < 9000){
+						if($money < 15000){
 							$player->sendMessage("§cYou dont have enough money!");
 							return true;
 						}
 					}
 					if($crank == "King"){
-						if($money > 9000){
-							EconomyAPI::getInstance()->reduceMoney($player, "9000");
+						if($money > 20000){
+							EconomyAPI::getInstance()->reduceMoney($player, "20000");
 							$papi->setGroup($player, $emperor);
 							$player->sendMessage("§bYour rank has been upgraded to §6Emperor");
 							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
 							return true;
 						}
-						if($money == 9000){
-							EconomyAPI::getInstance()->reduceMoney($player, "9000");
+						if($money == 20000){
+							EconomyAPI::getInstance()->reduceMoney($player, "20000");
 							$papi->setGroup($player, $emperor);
 							$player->sendMessage("§bYour rank has been upgraded to §6Emperor");
 							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
 							return true;
 						}
-						if($money < 9000){
+						if($money < 20000){
 							$player->sendMessage("§cYou dont have enough money!");
 							return true;
 						}
 					}
 					if($crank == "Emperor"){
-						if($money > 9000){
-							EconomyAPI::getInstance()->reduceMoney($player, "9000");
-							$papi->setGroup($player, $vip);
-							$player->sendMessage("§bYour rank has been upgraded to §6Vip");
-							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
-							return true;
-						}
-						if($money == 9000){
-							EconomyAPI::getInstance()->reduceMoney($player, "9000");
-							$papi->setGroup($player, $vip);
-							$player->sendMessage("§bYour rank has been upgraded to §6Vip");
-							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
-							return true;
-						}
-						if($money < 9000){
-							$player->sendMessage("§cYou dont have enough money!");
-							return true;
-						}
-					}
-				        if($crank == "Vip"){
-						if($money > 9000){
-							EconomyAPI::getInstance()->reduceMoney($player, "9000");
-							$papi->setGroup($player, $mvp);
-							$player->sendMessage("§bYour rank has been upgraded to §6Mvp");
-							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
-							return true;
-						}
-						if($money == 9000){
-							EconomyAPI::getInstance()->reduceMoney($player, "9000");
-							$papi->setGroup($player, $mvp);
-							$player->sendMessage("§bYour rank has been upgraded to §6Mvp");
-							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
-							return true;
-						}
-						if($money < 9000){
-							$player->sendMessage("§cYou dont have enough money!");
-							return true;
-						}
-					}
-					if($crank == "Mvp"){
-						if($money > 9000){
-							EconomyAPI::getInstance()->reduceMoney($player, "9000");
+						if($money > 50000){
+							EconomyAPI::getInstance()->reduceMoney($player, "50000");
 							$papi->setGroup($player, $dragon);
 							$player->sendMessage("§bYour rank has been upgraded to §6DRAGON");
 							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
 							return true;
 						}
-						if($money == 9000){
-							EconomyAPI::getInstance()->reduceMoney($player, "9000");
+						if($money == 50000){
+							EconomyAPI::getInstance()->reduceMoney($player, "50000");
 							$papi->setGroup($player, $dragon);
 							$player->sendMessage("§bYour rank has been upgraded to §6DRAGON");
 							$player->addTitle("§bUpgraded!", "§6Your rank has been upgraded!");
 							return true;
 						}
-						if($money < 9000){
+						if($money < 50000){
 							$player->sendMessage("§cYou dont have enough money!");
 							return true;
 						}
